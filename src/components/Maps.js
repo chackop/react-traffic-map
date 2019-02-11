3fbe6015aa1656ba01a8636b427ec2d165de9a97
Chacko <chackop@gmail.com>
2019-02-11 00:22:27 +0000

    markers

--- src/components/Maps.js
+++ src/components/Maps.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import ReactMapGL from "react-map-gl";
+import MapGL, { Marker } from "react-map-gl";
+import CityPin from "./Pin";
 
 export default class Maps extends Component {
   constructor(props) {
@@ -10,11 +11,21 @@ export default class Maps extends Component {
         width: 1100,
         height: 600,
         zoom: 4,
-        lat: 53.5444,
-        long: 113.4909,
+        latitude: 53.5444,
+        longitude: -113.4909
         // lat: 51.5074,
         // long: 0.1278
-      }
+      },
+      coords: [
+        {
+          lat: 53.5444,
+          long: -113.4909
+        },
+        {
+          lat: 53.567,
+          long: -113.4947
+        }
+      ]
     };
 
     this.toggle = this.toggle.bind(this);
@@ -27,12 +38,19 @@ export default class Maps extends Component {
   };
 
   render() {
+    const { coords } = this.state;
     return (
-      <ReactMapGL
+      <MapGL
         {...this.state.viewport}
         onViewportChange={viewport => this.setState({ viewport })}
         mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
-      />
+      >
+        {coords.map(coord => (
+          <Marker latitude={coord.lat} longitude={coord.long}>
+            <CityPin />
+          </Marker>
+        ))}
+      </MapGL>
     );
   }
 }
